cced58a8ca02bb9e81ef9c89edffe10c9d3f13e3
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-06-28 09:58:53 +0300

    test4

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require ('express')
 const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload')
 const ejs = require('ejs')
 const path = require('path')
+const fs = require('fs')
 const Photo = require('./models/Photo')
 
 const app = express()
@@ -20,11 +22,22 @@ app.set("view engine","ejs")
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(fileUpload());
 
 //ROUTES
-app.get('/',(req,res)=>{
-    res.render('index')
+app.get('/',async (req,res)=>{
+    const photos = await Photo.find({}).sort('-dateCreated')
+    res.render('index',{
+        photos
+    })
        
+})
+
+app.get('/photos/:id', async (req,res)=>{
+    const photo = await Photo.findById(req.params.id)
+    res.render('photo',{
+        photo
+    })
 })
 
 app.get('/about',(req,res)=>{
@@ -38,8 +51,23 @@ app.get('/add',(req,res)=>{
 })
 
 app.post('/photos',async (req,res)=>{
-   await Photo.create(req.body)
+
+    const uploadDir = 'public/uploads';
+    if(!fs.existsSync(uploadDir)){
+        fs.mkdirSync(uploadDir)
+    }
+
+ let uploadeImage = req.files.image
+ let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name
+
+ uploadeImage.mv(uploadPath,async()=> {
+    await Photo.create({
+        ...req.body,
+        image:'/uploads/' + uploadeImage.name
+    })
     res.redirect('/')
+ })
+  
 })
 
 
