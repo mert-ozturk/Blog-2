7e4c9d118bec9c5ffb1dc8228ef05fa833a0de46
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-06-30 04:49:47 +0300

    test5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ('express')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 const ejs = require('ejs')
 const path = require('path')
 const fs = require('fs')
@@ -12,6 +13,8 @@ const app = express()
 mongoose.connect('mongodb://localhost/cleanblogs',{
     useNewUrlParser:true,
     useUnifiedTopology:true,
+    
+
 })
 
 //TEMPENGİNE
@@ -23,7 +26,9 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(fileUpload());
-
+app.use(methodOverride('_method',{
+    methods:['POST','GET']
+}))
 //ROUTES
 app.get('/',async (req,res)=>{
     const photos = await Photo.find({}).sort('-dateCreated')
@@ -70,7 +75,30 @@ app.post('/photos',async (req,res)=>{
   
 })
 
+app.get('/photos/edit/:id',async (req,res)=>{
+    const photo =  await Photo.findOne({_id: req.params.id})
+    res.render('edit',{
+        photo,
+    })
+    
+})
 
+app.put('/photos/:id',async (req,res)=>{
+    const photo =  await Photo.findOne({_id: req.params.id})
+    photo.title = req.body.title
+    photo.description = req.body.description
+    photo.save()
+
+    res.redirect(`/photos/${req.params.id}`)
+})
+
+app.delete('/photos/:id',async  (req,res)=>{
+    const photo = await Photo.findByIdAndRemove(req.params.id)
+    let deletedImage = __dirname + '/public' + photo.image
+    fs.unlinkSync(deletedImage)
+    await Photo.findByIdAndRemove(req.params.id)
+    res.redirect('/')
+})
 
 const port = 3000
 app.listen(port,()=> {
