d49045d0dc269625ece3be65bbd0f14ebcafd7da
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-07-01 14:19:01 +0300

    test6

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require('ejs')
 const path = require('path')
 const fs = require('fs')
 const Photo = require('./models/Photo')
-
+const photoController = require('./controllers/photoControllers')
+const pageController = require('./controllers/pageController')
 const app = express()
 
 //connect DB
@@ -30,75 +31,14 @@ app.use(methodOverride('_method',{
     methods:['POST','GET']
 }))
 //ROUTES
-app.get('/',async (req,res)=>{
-    const photos = await Photo.find({}).sort('-dateCreated')
-    res.render('index',{
-        photos
-    })
-       
-})
-
-app.get('/photos/:id', async (req,res)=>{
-    const photo = await Photo.findById(req.params.id)
-    res.render('photo',{
-        photo
-    })
-})
-
-app.get('/about',(req,res)=>{
-    res.render('about')
-       
-})
-
-app.get('/add',(req,res)=>{
-    res.render('add')
-       
-})
-
-app.post('/photos',async (req,res)=>{
-
-    const uploadDir = 'public/uploads';
-    if(!fs.existsSync(uploadDir)){
-        fs.mkdirSync(uploadDir)
-    }
-
- let uploadeImage = req.files.image
- let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name
-
- uploadeImage.mv(uploadPath,async()=> {
-    await Photo.create({
-        ...req.body,
-        image:'/uploads/' + uploadeImage.name
-    })
-    res.redirect('/')
- })
-  
-})
-
-app.get('/photos/edit/:id',async (req,res)=>{
-    const photo =  await Photo.findOne({_id: req.params.id})
-    res.render('edit',{
-        photo,
-    })
-    
-})
-
-app.put('/photos/:id',async (req,res)=>{
-    const photo =  await Photo.findOne({_id: req.params.id})
-    photo.title = req.body.title
-    photo.description = req.body.description
-    photo.save()
-
-    res.redirect(`/photos/${req.params.id}`)
-})
-
-app.delete('/photos/:id',async  (req,res)=>{
-    const photo = await Photo.findByIdAndRemove(req.params.id)
-    let deletedImage = __dirname + '/public' + photo.image
-    fs.unlinkSync(deletedImage)
-    await Photo.findByIdAndRemove(req.params.id)
-    res.redirect('/')
-})
+app.get('/',photoController.getAllPhotos)
+app.get('/photos/:id',photoController.getPhoto)
+app.get('/about',pageController.getAboutPage)
+app.get('/add',pageController.getAddPage)
+app.post('/photos',photoController.createPhoto)
+app.get('/photos/edit/:id',pageController.getEditPage)
+app.put('/photos/:id',photoController.updatePhoto)
+app.delete('/photos/:id',photoController.deletePhoto)
 
 const port = 3000
 app.listen(port,()=> {
